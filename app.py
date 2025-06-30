eddb862c8c747177d62d4e69004b0672caa034c4
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-06-30 17:10:27 +0800

    Modify app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes import todo_bp
 
 app = Flask(__name__)
-app.register_blueprint(todo_bp, url_prefix='/tasks')
+app.register_blueprint(todo_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
