80d9762cde861ee9ff2cdd6088e57878dcc7780f
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-07-01 23:10:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from routes import todo_bp
+from db import init_db
 
-app = Flask(__name__)
-app.register_blueprint(todo_bp)
+def create_app():
+    app = Flask(__name__)
+    
+    app.register_blueprint(todo_bp)
+    
+    init_db()
+    
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
