e69b4f4d33efe2df87429e636f98df22ca39f802
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-07-03 22:23:17 +0800

    Add 404 error handler in app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes import todo_bp
 from db import init_db
 
 HTTP_INTERNAL_SERVER_ERROR = 500
+HTTP_NOT_FOUND = 400
 
 def create_app():
     app = Flask(__name__)
@@ -17,8 +18,12 @@ def create_app():
         'message': 'Welcome! This TODO API is developed by Team PyQt5.'
     })
     
+    @app.errorhandler(404)
+    def handle_not_found_error(e):
+        return jsonify({'error': 'Not Found'}), HTTP_NOT_FOUND
+
     @app.errorhandler(Exception)
-    def handle_error(e):
+    def handle_internal_server_error(e):
         return jsonify({
             'error': 'Internal Server Error'
         }), HTTP_INTERNAL_SERVER_ERROR
